f9cf26109ac127780ed8504c2be3d1b7c9a0043b
Pieterjan De Clippel <pieterjandeclippel@hotmail.com>
2020-02-25 12:39:21 +0100

    Added minify property on NgxJsonLdComponent

--- projects/ngx-json-ld/src/ngx-json-ld.component.ts
+++ projects/ngx-json-ld/src/ngx-json-ld.component.ts
@@ -12,17 +12,46 @@ import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class NgxJsonLdComponent {
-  @Input()
-  set json(currentValue: {}) {
-    this.jsonLD = this.getSafeHTML(currentValue);
+
+  //#region Json
+  private _json: any = {};
+  get json() {
+    return this._json;
   }
+  @Input() set json(value: {}) {
+    this._json = value;
+    this.updateJsonLd();
+  }
+  //#endregion
+  //#region Minify
+  private _minify: boolean = false;
+  get minify() {
+    return this._minify;
+  }
+  @Input() set minify(value: boolean) {
+    this._minify = value;
+    this.updateJsonLd();
+  }
+  //#endregion
+
   @HostBinding('innerHTML') jsonLD: SafeHtml;
   constructor(private sanitizer: DomSanitizer) {}
 
-  getSafeHTML(value: {}) {
-    const json = value
-      ? JSON.stringify(value, null, 2).replace(/<\/script>/g, '<\\/script>')
-      : '';
+  private updateJsonLd() {
+    this.jsonLD = this.getSafeHTML(this._json, this._minify);
+  }
+
+  private getSafeHTML(value: {}, minify: boolean) {
+    let json = null;
+    if (minify === true) {
+      json = value
+        ? JSON.stringify(value).replace(/<\/script>/g, '<\\/script>')
+        : '';
+    } else {
+      json = value
+        ? JSON.stringify(value, null, 2).replace(/<\/script>/g, '<\\/script>')
+        : '';
+    }
     const html = `<script type="application/ld+json">${json}</script>`;
     return this.sanitizer.bypassSecurityTrustHtml(html);
   }
